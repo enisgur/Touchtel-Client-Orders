63750bc647838bad3d7cb3653c914faf40a9a493
enisgur <gurenis@outlook.com>
2020-09-28 21:18:55 -0700

    first commit

---
+++ src/reducers/index.js
@@ -0,0 +1,14 @@
+import { combineReducers } from "redux";
+import alert from "./alert";
+// import auth from './auth';
+// import profile from './profile';
+// import products from './products';
+// import fix from './fix';
+
+export default combineReducers({
+  alert,
+  //   auth,
+  //   profile,
+  //   products,
+  //   fix
+});
